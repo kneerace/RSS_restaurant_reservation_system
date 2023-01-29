ba6bbbd8a3a00b44872aea30b2d363b13afa40ee
kneerace <kneerace@gmail.com>
2023-01-29 07:30:09 -0500

    FrontEnd - starter code upload
    starter code regarding FrontEnd uploaded, from Thinkful

---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "./NotFound";
+import { today } from "../utils/date-time";
+
+/**
+ * Defines all the routes for the application.
+ *
+ * You will need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard date={today()} />
+      </Route>
+
+      <Route path="/search">
+        <h1> Search page</h1>
+      </Route>
+      <Route path="/reservations/new">
+        <h1> New Reservation page</h1>
+      </Route>
+      <Route path="/tables/new">
+        <h1>New table page</h1>
+      </Route>
+
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
