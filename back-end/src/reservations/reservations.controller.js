1ce7aac151ee133ae99eb2bd853f56401828ee3d
kneerace <kneerace@gmail.com>
2023-02-04 14:39:15 -0500

    list function modified for data from reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,10 +1,11 @@
+const reservationsService = require("./reservations.service");
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+async function list(req, res, next) {
+      reservationsService.list()
+      .then((data)=> res.json({data}))
+      .catch(next);
 }
 
 module.exports = {
