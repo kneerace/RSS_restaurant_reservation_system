a4a65c7c41369cb7639ae44df7dcae56ed0e098b
kneerace <kneerace@gmail.com>
2023-02-08 07:02:32 -0500

    ReservationForm:handleChange, handleSubmit, handleCancel added

--- front-end/src/reservations/ReservationsForm.js
+++ front-end/src/reservations/ReservationsForm.js
@@ -1,12 +1,69 @@
 import React, { useState, useEffect} from "react";
 import {useHistory, useLocation} from "react-router-dom";
+import { createReservation } from "../utils/api";
 
 function ReservationForm(){
+  const history = useHistory();
+  const {pathname} = useLocation();
+
+  const today = new Date().toISOString().split("T")[0];
+  const initialReservation = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    people: 1,
+    reservation_date: today,
+    reservation_time: "10:30",
+  }
+  const [reservation, setReservation] = useState(initialReservation);
+  //show initial form data
+  useEffect(()=>{
+    const abortController = new AbortController();
+    function newReservation(){
+      if(pathname.includes("new")){
+          setReservation(initialReservation);
+      }
+      // else{
+          // setEditFlag(false);
+      // }
+    }
+    newReservation();
+    },[pathname]);
+    
+    // record of change on filling form values
+    function handleChange({target}){
+      if(target.name === "people"){
+        setReservation({...reservation, [target.name]:Number(target.value)});
+      }
+      else {
+        setReservation({...reservation, [target.name]: target.value});
+      }
+      console.log('handleChange ', reservation);
+    };
+
+    // effect based on SUBMIT button click
+    async function handleSubmit(event){
+      event.preventDefault();
+      try{
+        const abortController = new AbortController();
+        const response = await createReservation(reservation, abortController.abort());
+        history.push("/"); // for now pusing to dashboard
+      } catch(error){
+        console.log("handleSubmit error ", error)
+      }
+    }
+
+    // effect based on CANCEL button click
+    function handleCancel(event){
+      event.preventDefault();
+      history.goBack();
+    }
+
     return (
         <div>
     <h1>New Reservation</h1>
 
-    <form >
+    <form onSubmit={handleSubmit}>
         <div className="row d-flex justify-content-center">
             <div className="col-md-4 col-lg-4 mb-3">
                 <label htmlFor="first_name" className="form-label">
@@ -16,7 +73,8 @@ function ReservationForm(){
                     name="first_name" 
                     id="first_name"
                     placeholder="First Name"
-                    classname="form-control" required
+                    classname="form-control" 
+                    onChange={handleChange} required
                 />
             </div>
 
@@ -28,7 +86,8 @@ function ReservationForm(){
                     name="last_name" 
                     id="last_name"
                     placeholder="Last Name"
-                    classname="form-control" required
+                    classname="form-control" 
+                    onChange={handleChange} required
                 />
             </div>
         </div>
@@ -43,21 +102,23 @@ function ReservationForm(){
               name="mobile_number"
               id="mobile_number"
               placeholder="(571)-456-7890"
-              className="form-control" required
+              className="form-control" 
+              onChange={handleChange} required
             />
           </div>
           <div className="col-md-4 col-lg-4 mb-3">
-            <label htmlFor="guests" className="form-label">
+            <label htmlFor="people" className="form-label">
               Number of Guests
             </label>
             <input
               type="number"
               min="1"
               max="100"
-              name="guests"
-              id="guests"
+              name="people"
+              id="people"
               placeholder="1"
-              className="form-control" required
+              className="form-control" 
+              onChange={handleChange} required
             />
           </div>
         </div>
@@ -72,7 +133,8 @@ function ReservationForm(){
               name="reservation_date"
               id="reservation_date"
               placeholder="YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}"
-              className="form-control" required
+              className="form-control" 
+              onChange={handleChange} required
             />
           </div>
           <div className="col-md-4 col-lg-4 mb-3">
@@ -84,7 +146,8 @@ function ReservationForm(){
               name="reservation_time"
               id="reservation_time"
               placeholder="HH:MM" pattern="[0-9]{2}:[0-9]{2}"
-              className="form-control" required
+              className="form-control" 
+              onChange={handleChange} required
             />
           </div>
         </div>
@@ -96,7 +159,8 @@ function ReservationForm(){
             </button>
           </div>
           <div className="col-lg-2 col-md-2  col-sm col-6 col">
-            <button type="button" className="btn btn-secondary form-control">
+            <button type="button" className="btn btn-secondary form-control"
+            onClick={()=> handleCancel()}>
                 Cancel
             </button>
           </div>
