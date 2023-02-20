0e1a73c30744b61291bf045771ca90909150f5cd
kneerace <kneerace@gmail.com>
2023-02-19 23:58:41 -0500

    ErrorAlert.js:  handling of error fade on clicing X

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 /**
  * Defines the alert message to render if the specified error is truthy.
@@ -9,11 +9,38 @@ import React from "react";
  */
 
 function ErrorAlert({ error }) {
+  const [ showError, setShowError]= useState("hide");
+
+  console.log(' CheckError ', error)
+  useEffect (()=>{
+    if(error){
+      setShowError("show");
+    }
+    else { setShowError("hide");
+  }
+  },[error]);
+
+   function handleShowError(){
+      setShowError("hide")
+    console.log("handleShowError:::: ", showError )
+  }
+
+  console.log("out of handleShowError:::: ", showError )
+
   return (
     error && (
-      <div className="alert alert-danger m-2">Error: {error.message}</div>
+      
+      <div className={`alert alert-danger m-2 alert-dismissible fade ${showError}`} role="alert">
+        <strong>Error: </strong>{error.message}
+        {showError === "show" ? (
+        <button type="button" className="close" aria-label="Close" onClick={handleShowError}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+          ): null}
+      </div>
+      )
     )
-  );
-}
+  }
+  
 
 export default ErrorAlert;
