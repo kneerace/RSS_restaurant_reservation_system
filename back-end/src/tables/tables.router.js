116c4fc27467b8606a1af9c23c0a6802ceed649e
kneerace <kneerace@gmail.com>
2023-02-25 00:21:26 -0500

    tables.router:put-update added

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -11,6 +11,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed")
 router.route("/")
     .get(controller.list)
     .post(controller.create)
+    .put(controller.update)
     .all(methodNotAllowed);
 
 
