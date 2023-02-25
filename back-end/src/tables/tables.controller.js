27c15ea02f64bab3a59875d52842c9d5ddb09e12
kneerace <kneerace@gmail.com>
2023-02-25 00:20:43 -0500

    tables.controller:update and its checks added

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,10 +1,10 @@
-const tableService = require("./tables.service");
+const tablesService = require("./tables.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 /**
  * List handler for tables list
  */
 async function list(req, res) {
-  const data = await tableService.list();
+  const data = await tablesService.list();
     res.json({data});
   }
 
@@ -27,16 +27,18 @@ async function requestHasData(req, res, next){
 // check if request has property and value
 function reqBodyHas(propertyName){
   return function(req, res, next){
+    // console.log("reqBodyHas:: req  ", req.body)
       const {data ={} } = req.body;
+      // console.log("reqBodayHas:::  ", data)
       if(data[propertyName]){
           return next();
       }
       next({ status: 400, 
-          message : `Reservation request must include "${propertyName}"`
+          message : `Table request must include "${propertyName}"`
       });
   };
 }
-// -------TABLE NAME CHECK
+// -------TABLE CHECK
   //  1.check if table_name is valid 
 function isTableNameValid(req, res, next){
    res.locals.table_name = req.body.data.table_name;
@@ -50,9 +52,7 @@ function isTableNameValid(req, res, next){
   next();
 }
 
-  // CAPACITY CHECK 
-  // 1. is capacity valid 
-   //  1.check if table_name is valid 
+  // 2. is capacity valid 
 function isCapacityValid(req, res, next){
     res.locals.capacity = req.body.data.capacity;
    
@@ -64,15 +64,35 @@ function isCapacityValid(req, res, next){
    }
    next();
  }
-  
+ // 3. do table exists 
+async function tableIdExists(req, res, next){
+  const {table_id} = req.body.data;
+  const table = await tablesService.read(table_id);
+  if(table){
+    res.locals.table_id = table.table_id;
+    return next();
+  }
+  next({
+    status: 404, 
+    message: `Table with ID ${table_id} does not exist`,
+  });
+};
 
 // ---- CREATE
  async function create(req, res, next){
   const tableDetails = req.body.data;
-  tableService.create(tableDetails)
+  tablesService.create(tableDetails)
       .then((data)=> res.status(201).json({data}))
       .catch(next); 
 }
+
+// updte table
+async function update(req, res, next){
+  const table_id = res.locals.table_id
+  await tablesService.update(table_id);
+  res.status(200).json({});
+}
+
 
 module.exports = {
   list: asyncErrorBoundary(list),
@@ -83,6 +103,11 @@ module.exports = {
     isTableNameValid,
     isCapacityValid,
     asyncErrorBoundary(create),
-  ], 
+  ],
+  update:[
+    reqBodyHas("table_id"), 
+    tableIdExists,
+    asyncErrorBoundary(update),
+  ]
   
 };
