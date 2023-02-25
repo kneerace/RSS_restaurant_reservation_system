46ad4e2cf196986080ea73c6a538ea9662b64474
kneerace <kneerace@gmail.com>
2023-02-25 00:21:54 -0500

    tables.service: read and update added

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -7,13 +7,29 @@ function list(){
         .orderBy("table_id");
 }
 
+function read(table_id){
+    return knex("tables")
+        .select("*")
+        .where({table_id})
+        .first();
+}
+
 function create(newTable){
     return knex("tables")
     .insert(newTable)
     .returning("*")
     .then((createdTable)=> createdTable[0]);
 }
+
+function update(table_id){
+    return knex("tables")
+        .where ({table_id})
+        .update({reservation_id:null})
+        .returning("*");
+}
 module.exports = {
     list,
     create,
+    read,
+    update,
 }
