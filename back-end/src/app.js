58bc975e7e9d74bd36b2dadd98de3dca17b99806
kneerace <kneerace@gmail.com>
2023-02-27 01:19:15 -0500

    app.js: tableRouter updated as tablesRouter

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const reservationsRouter = require("./reservations/reservations.router");
-const tableRouter = require("./tables/tables.router");
+const tablesRouter = require("./tables/tables.router");
 const { table } = require("console");
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
-app.use("/tables", tableRouter)
+app.use("/tables", tablesRouter)
 
 app.use(notFound);
 app.use(errorHandler);
