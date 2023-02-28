aa346ffa72f4d63622b9085bf7382d5cec3e08fa
kneerace <kneerace@gmail.com>
2023-02-28 15:43:10 -0500

    reservations.service:knex for update added

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -47,13 +47,27 @@ function create(newReservation){
         .insert(newReservation)
         .returning("*")
         .then(returnedValue => returnedValue[0]);
-
 }
 
+
+function update(reservationDetails){
+    return knex("reservations")
+        .where({reservation_id : reservationDetails.reservation_id})
+        .update({
+            first_name: reservationDetails.first_name,
+            last_name: reservationDetails.last_name,
+            mobile_number: reservationDetails.mobile_number,
+            reservation_date: reservationDetails.reservation_date,
+            reservation_time: reservationDetails.reservation_time, 
+            people: reservationDetails.people
+        },"*");
+
+}
 module.exports = {list,
     listByDate,
     listByMobileNumber,
     read,
     create,
     updateStatus,
+    update
     };
